3a267248e59c57eeaf08cb369f15afbb10b79c68
hamo80577 <mohamed.mahmoud.412860_ibs.ext@talabat.com>
2026-05-21 09:42:08 +0300

    Update access-control.module.ts

--- apps/api/src/access-control/access-control.module.ts
+++ apps/api/src/access-control/access-control.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 
+import { UsersModule } from "../users/users.module";
 import { AccessControlController } from "./access-control.controller";
 import { AccessPolicyService } from "./access-policy.service";
 
 @Module({
+  imports: [JwtModule.register({}), UsersModule],
   controllers: [AccessControlController],
   providers: [AccessPolicyService],
   exports: [AccessPolicyService]
